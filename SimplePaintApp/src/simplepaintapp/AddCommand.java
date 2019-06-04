36cefb2a47b4299013670929cba8a40ec408c887
sjimmie bleeker <blee1703@student.nhl.nl>
2019-06-04 11:42:00 +0200

    -useless folder

--- SimplePaintApp/src/simplepaintapp/AddCommand.java
+++ SimplePaintApp/src/simplepaintapp/AddCommand.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package simplepaintapp;
 
 import java.awt.Point;
 import java.util.ArrayList;
 
 
-/**
- *
- * @author Sjimmie
- */
 public class AddCommand implements Command{
     private DrawAbleShape selectedShape;
     private ArrayList<DrawAbleShape> shapes;
